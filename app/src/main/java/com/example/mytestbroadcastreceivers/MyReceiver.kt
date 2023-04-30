bfbfccc17a4055f852bf82d8f8b04678a4dc8c31
VladJu <juikovvlad@gmail.com>
2023-04-30 14:07:46 +0300

    Processing multiple messages/ registration(static/dynamic)

--- app/src/main/java/com/example/mytestbroadcastreceivers/MyReceiver.kt
+++ app/src/main/java/com/example/mytestbroadcastreceivers/MyReceiver.kt
@@ -6,12 +6,18 @@ import android.content.Intent
 import android.widget.Toast
 
 class MyReceiver : BroadcastReceiver() {
-
     override fun onReceive(context: Context?, intent: Intent?) {
-        //каждая система содержит какой то экшен, для того что бы было понятно что произошло
-        when(intent?.action){
-            Intent.ACTION_AIRPLANE_MODE_CHANGED ->{
-                Toast.makeText(context,"Airplane mode changed",Toast.LENGTH_SHORT).show()
+        when (intent?.action) {
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
+            }
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                //получаем значение которое передается системой
+                val turnedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(
+                    context, "Airplane mode changed. Turned on: $turnedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
